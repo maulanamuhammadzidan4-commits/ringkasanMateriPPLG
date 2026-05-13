5cb2c3f49bbfff608e1d50014b383fea0cf800f9
maulanamuhammadzidan4-commits <maulanamuhammadzidan4@gmail.com>
2026-05-13 08:56:19 +0700

    penambahan script sama tag video

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 70, // Menyesuaikan tinggi navbar
+                behavior: 'smooth'
+            });
+        }
+    });
+});
